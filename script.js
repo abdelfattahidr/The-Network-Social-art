f54d3665ebdcaec9de8a3405000d0bbfb6a990f0
idrcoding <abdelfattahidr122@gmail.com>
2022-09-12 11:20:21 +0100

    The Network Social Art

---
+++ script.js
@@ -0,0 +1,25 @@
+function myInstagram() {
+    window.open("https://www.instagram.com");
+}
+
+function myFacebook() {
+    window.open("https://www.facebook.com");
+}
+
+function myTwitter() {
+    window.open("https://www.twitter.com");
+}
+
+function myReddit() {
+    window.open("https://www.reddit.com");
+}
+
+function mySnapchat() {
+    window.open("https://www.snapchat.com");
+}
+
+function myYoutube() {
+    window.open("https://www.youtube.com");
+}
+
+/* create by IDR */
